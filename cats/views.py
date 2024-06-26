dcbfd2e8039c08592f0bbd2bf75c6f7e10a85e47
Aleksandr Rakov <alexrinko@hotmail.com>
2024-06-27 02:56:59 +0500

    Свой вьюсет и миксины

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Cat, Owner
 from .serializers import CatListSerializer, CatSerializer, OwnerSerializer
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatListSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
